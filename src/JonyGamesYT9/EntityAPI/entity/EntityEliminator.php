828bcb2312ee05ac100149f2ec2859751e0b6fa2
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2021-08-24 16:24:53 -0500

    update EntityEliminator.php

--- src/JonyGamesYT9/EntityAPI/entity/EntityEliminator.php
+++ src/JonyGamesYT9/EntityAPI/entity/EntityEliminator.php
@@ -1,1 +1,46 @@
+<?php
 
+namespace JonyGamesYT9\EntityAPI\entity;
+
+use JonyGamesYT9\EntityAPI\entity\types\NPC;
+use pocketmine\Player;
+use pocketmine\Server;
+
+/**
+* Class EntityEliminator
+* @package JonyGamesYT9\EntityAPI\entity
+*/
+class EntityEliminator
+{
+
+  /**
+  * @param Player $player
+  * @return void
+  */
+  public function eliminate(Player $player, string $name): void
+  {
+    foreach ($player->getLevel()->getEntities() as $entities) {
+      if ($entities instanceof NPC) {
+        if ($entities->getName() === $name) {
+          $entities->getCompoundTag()->removeTag("Name");
+          $entities->kill();
+        }
+      }
+    }
+  }
+
+  /**
+  * @return void
+  */
+  public function eliminateAll(): void
+  {
+    foreach (Server::getInstance()->getLevels() as $levels) {
+      foreach ($levels->getEntities() as $entities) {
+        if ($entities instanceof NPC) {
+          $entities->getCompoundTag()->removeTag("Name");
+          $entities->kill();
+        }
+      }
+    }
+  }
+}
