5c2adecad65f0ad2861a56eafb6b36bd7c037410
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2021-08-24 15:58:02 -0500

    update NPC.php

--- src/JonyGamesYT9/EntityAPI/entity/types/NPC.php
+++ src/JonyGamesYT9/EntityAPI/entity/types/NPC.php
@@ -1,1 +1,56 @@
+<?php
 
+namespace JonyGamesYT9\EntityAPI\entity\types;
+
+use pocketmine\level\Level;
+use pocketmine\nbt\tag\CompoundTag;
+
+/**
+* Class NPC
+* @package JonyGamesYT9\EntityAPI\entity\types
+*/
+class NPC extends \pocketmine\entity\Human
+{
+
+  /**
+  * NPC constructor.
+  * @param Level $level
+  * @param CompoundTag $nbt
+  */
+  public function __construct(Level $level, CompoundTag $nbt)
+  {
+    parent::__construct($level, $nbt);
+  }
+
+  /**
+  * @return string
+  */
+  public function getName(): string
+  {
+    if ($this->getCompoundTag()->hasTag("Name")) {
+      return $this->getCompoundTag()->getString("Name");
+    } else {
+      return "";
+    }
+  }
+
+  /**
+  * @return string
+  */
+  public function getFloatScale(): float
+  {
+    if ($this->getCompoundTag()->hasTag("Scale")) {
+      return $this->getCompoundTag()->getFloat("Scale");
+    } else {
+      return 1.0;
+    }
+  }
+
+  /**
+  * @return CompoundTag
+  */
+  public function getCompoundTag(): CompoundTag
+  {
+    return $this->namedtag;
+  }
+}
